535a6a7df0fc3f571041953e7e8faa967e41bd09
JoseTH00 <joseoscarthorlet@gmail.com>
2024-10-07 08:54:34 -0300

    resolucion del ej8avanzadosPOO

--- src/Prog2problemas/EjAvanzadosPOO/Ej1Circle.java
+++ src/Prog2problemas/EjAvanzadosPOO/Ej1Circle.java
@@ -1,7 +1,8 @@
 package Prog2problemas.EjAvanzadosPOO;
 
 class Circle {
-     private double radius=1.0;
+    private double radius=1.0;
+    private String color="red";
 
     public Circle() {
     }
@@ -10,12 +11,25 @@ class Circle {
         this.radius = radius;
     }
 
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
     public double getRadius() {
         return radius;
     }
 
     public void setRadius(double radius) {
         this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
 
     public double getArea() {
@@ -28,8 +42,9 @@ class Circle {
 
     @Override
     public String toString() {
-        return "Circlee{" +
+        return "Circle{" +
                 "radius=" + radius +
+                ", color='" + color + '\'' +
                 '}';
     }
 
