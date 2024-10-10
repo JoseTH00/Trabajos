3600c4bafaec8373167c81f550c4f641430bd385
JoseTH00 <joseoscarthorlet@gmail.com>
2024-10-09 21:54:56 -0300

    resolucion ej9avanzadosPOO

--- src/Prog2problemas/Cualquiera.java
+++ src/Prog2problemas/Cualquiera.java
@@ -1,5 +1,15 @@
 package Prog2problemas;
 
-public class Cualquiera {
+class Cualquiera {
+    public static void main(String[] args) {
+        String s1 = "Hola";
+        String s2 = "Hola";
+        String s3 = new String("Hola");
+        System.out.println(s1=s2);
+        System.out.println(s1=s3);
+
+    }
 }
+
+
 
---
+++ src/Prog2problemas/EjAvanzadosPOO/Ej9School.java
@@ -0,0 +1,121 @@
+package Prog2problemas.EjAvanzadosPOO;
+
+class Person {
+    private String name, address;
+
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
+
+class Staff extends Person {
+    private String school;
+    private double pay;
+
+    public Staff(String name, String address, String school, double pay) {
+        super(name, address);
+        this.school = school;
+        this.pay = pay;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public double getPay() {
+        return pay;
+    }
+
+    public void setPay(double pay) {
+        this.pay = pay;
+    }
+
+    @Override
+    public String toString() {
+        return "Staff{" +super.toString()+
+                " school='" + school + '\'' +
+                ", pay=" + pay +
+                '}';
+    }
+}
+
+class Student extends Person {
+    private String program;
+    private int year;
+    private double fee;
+
+    public Student(String name, String address, String program, int year, double fee) {
+        super(name, address);
+        this.program = program;
+        this.year = year;
+        this.fee = fee;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getFee() {
+        return fee;
+    }
+
+    public void setFee(double fee) {
+        this.fee = fee;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +super.toString()+
+                " program='" + program + '\'' +
+                ", year=" + year +
+                ", fee=" + fee +
+                '}';
+    }
+}
+
+class School {
+    public static void main(String[] args) {
+        Staff staffMember = new Staff("Juan", "Calle Falsa 123", "Escuela Secundaria", 3000.0);
+        Student student = new Student("Ana", "Avenida Siempre Viva 456", "Ingeniería", 2, 1500.0);
+
+        System.out.println(staffMember);
+        System.out.println(student);
+    }
+}
